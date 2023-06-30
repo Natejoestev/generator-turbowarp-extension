d842fdcda4842e634e6eab35b2e99f6db71fb123
Natejoestev <85044853+Natejoestev@users.noreply.github.com>
2023-06-30 13:50:17 -0700

    refactor prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,90 +17,85 @@ module.exports = class extends Generator {
     }
 
     async prompting() {
-        const {extName} = await this.prompt([
-            { name: 'extName', message: 'Extension name:', default:this.appname }
-        ]);
-        this.extName = extName;
-        const {extId} = await this.prompt([
-            { name: 'extId', message: 'Extension Id:', default:extName.toLowerCase().match(/[a-z0-9]/g).join(''),
+        const usePkgManager = ({language, vscodeInit, serverType}) => {
+            return language == 'ts' ||
+                (vscodeInit['httpserver'] && serverType=='express');
+        };
+        const {
+            extName,extId, newFolder,
+            language,srcPath,
+            vscodeInit,browserType,serverType,devPort,
+            pkgManager
+        } = await this.prompt([
+            { name: 'extName', message: 'Extension name:', default:this.appname },
+            { name: 'extId', message: 'Extension Id:',
+            default: ({extName}) => extName.toLowerCase().match(/[a-z0-9]/g).join(''),
             validate(txt) {
                 const res = txt.match(/[a-z0-9]/g);
                 if (res && res.join('') == txt) return true;
                 return `Only a-z and 0-9 (no uppercase letters or special characters)`;
-            } }
-        ] );
-        this.extId = extId;
-        const {newFolder} = await this.prompt([
-            { name: 'newFolder', message: `Create in new folder?`, type:'expand', choices: [
-                { key: 'y', name: `Create a new folder (${extName}).`, value: true, short: 'Yes' },
-                { key: 'n', name: 'Populate current folder', value: false, short: 'No' }
-            ], default: 0 }
-        ]);
-        this.newFolder = newFolder;
+            } },
 
-        const {language} = await this.prompt([
+            { name: 'newFolder', message: `Create in new folder?`, type:'expand',
+            choices: ({extName}) => [
+                    { key: 'y', name: `Create a new folder (${extName}).`, value: true, short: 'Yes' },
+                    { key: 'n', name: 'Populate current folder', value: false, short: 'No' }
+            ], default: 0 },
+
             { name: 'language', type: 'list', choices: [
                 { name: 'javascript', value: 'js' },
                 { name: 'typescript (node project compiled to js)', value: 'ts' }   
-            ] }
+            ] },
+            { name: 'srcPath', message: 'What path to src files?', default:'src',
+            when: ({language}) => language == 'ts'
+            },
+
+            //NOTE maybe have support for other editors
+            { name: 'vscodeInit' , message: 'Initialize vscode dev env?', type:'checkbox',
+            choices({language}) {
+                return [
+                    { name: 'Launch dev browser', value: 'browser' },
+                    { name: 'Run dev HTTP server on startup', value: 'httpserver' }
+                    //NOTE maybe add 'hide unnecessary files' option (settings.json:explorer.exclude)
+                ].concat(language!='ts' ? []:
+                    { name: 'Run typescript compiler on startup', value: 'tsc' }
+                );
+            },
+            filter: (ans) => Object.fromEntries(ans.map(k => [k,true]))
+            },
+            { name: 'browserType', message: 'What browser to use?', type:'list', choices: [
+                { name: 'Chrome', value: 'chrome' },
+                { name: 'Edge', value: 'msedge' }
+            ],
+            when: ({vscodeInit}) => vscodeInit['browser']
+            },
+            { name: 'serverType', message: 'What http server to use?', type:'list', choices: [
+                { name: 'Python http.server', value: 'py' },
+                { name: 'Node express server', value: 'express' }
+            ],
+            when: ({vscodeInit}) => vscodeInit['httpserver']
+            },
+            { name: 'devPort' , message: 'Dev Http port:', default: '5010',
+            when: ({vscodeInit}) => vscodeInit['httpserver'] || vscodeInit['browser']
+            },
+
+            { name: 'pkgManager', message: 'What package manager to use?', type: 'list', choices: [
+                'npm'
+            ], when: usePkgManager }
         ]);
+        this.extName = extName;
+        this.extId = extId;
+        this.newFolder = newFolder;
         this.language = language;
-        if (language == 'ts') {
-            const {srcPath} = await this.prompt([
-                { name: 'srcPath', message: 'What path to src files?', default:'src' }
-            ]);
-            this.srcPath = srcPath;
-        }
-
-        //NOTE maybe have support for other editors
-        const vscodeChoices = [
-            { name: 'Launch dev browser', value: 'browser' },
-            { name: 'Run dev HTTP server on startup', value: 'httpserver' }
-            //NOTE maybe add 'hide unnecessary files' option (settings.json:explorer.exclude)
-        ];
-        if (language == 'ts') vscodeChoices.push({ name: 'Run typescript compiler on startup', value: 'tsc' });
-        const {vscodeInit} = await this.prompt([
-            { name: 'vscodeInit' , message: 'Initialize vscode dev env?', type:'checkbox', choices: vscodeChoices,
-            filter(ans) {
-                return Object.fromEntries(ans.map(k => [k,true]))
-            } }
-        ]);
+        this.srcPath = srcPath;
         this.vscodeInit = vscodeInit;
-        if (vscodeInit['browser']) {
-            const {browserType} = await this.prompt([
-                { name: 'browserType', message: 'What browser to use?', type:'list', choices: [
-                    { name: 'Chrome', value: 'chrome' },
-                    { name: 'Edge', value: 'msedge' }
-                ] }
-            ]);
-            this.browserType = browserType;
-        }
-        if (vscodeInit['httpserver']) {
-            const {serverType} = await this.prompt([
-                { name: 'serverType', message: 'What http server to use?', type:'list', choices: [
-                    { name: 'Python http.server', value: 'py' },
-                    { name: 'Node express server', value: 'express' }
-                ]}
-            ]);
-            this.httpServerType = serverType;
-        }
-        if (vscodeInit['httpserver'] || vscodeInit['browser']) {
-            const {devPort} = await this.prompt([
-                { name: 'devPort' , message: 'Dev Http port:', default: '5010' }
-            ]);
-            this.devPort = devPort;
-        }
+        this.browserType = browserType;
+        this.serverType = serverType;
+        this.devPort = devPort;
+        this.pkgManager = pkgManager;
 
-        this.usesPkgManager = language == 'ts' || (vscodeInit['httpserver'] && this.httpServerType=='express');
-
-        if (this.usesPkgManager) {
-            const {pkgManager} = await this.prompt([
-                { name: 'pkgManager', message: 'What package manager to use?', type: 'list', choices: [
-                    'npm'
-                ]}
-            ]);
-            this.pkgManager = pkgManager;
-        }
+        //REMAKE somehow pass whole "answer hash"
+        this.usesPkgManager = usePkgManager({language, vscodeInit, serverType});
     }
 
     configuring () {
@@ -113,13 +108,13 @@ module.exports = class extends Generator {
         if (this.vscodeInit['httpserver'] || this.vscodeInit['tsc']) {
             const content = this.fs.readJSON(this.templatePath('vscode', 'tasks.json'));
             if (this.vscodeInit['httpserver']) {
-                if (this.httpServerType == 'py') {
+                if (this.serverType == 'py') {
                     content.tasks.push(
                         JSON.parse(this.fs.read(
                             this.templatePath('vscode', 'http-python-task.json')
                         ).replaceAll('<%= devPort %>', this.devPort))
                     );
-                } else if (this.httpServerType == 'express') {
+                } else if (this.serverType == 'express') {
                     content.tasks.push(this.fs.readJSON(
                         this.templatePath('vscode', 'http-express-task.json')
                     ));
@@ -185,7 +180,7 @@ module.exports = class extends Generator {
                 this.spawnCommand('npm', ['install', 'typescript', '@turbowarp/types', '-y', '--quiet']);
             }
         }
-        if (this.vscodeInit['httpserver'] && this.httpServerType == 'express') {
+        if (this.vscodeInit['httpserver'] && this.serverType == 'express') {
             if (this.pkgManager=='npm') {
                 this.spawnCommand('npm', ['install', 'express', '-y', '--quiet']);
             }
